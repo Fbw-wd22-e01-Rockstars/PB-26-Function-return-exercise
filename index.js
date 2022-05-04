4c3bfbd2612ebc6b4248d7a600f81b889cc45564
Naqvi <44436701+nrcool@users.noreply.github.com>
2022-05-04 12:25:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// The following function returns true if the parameter age is greater than 18.
+
+// Otherwise returns false:
+
+
+function checkAge(age) {
+
+  }
+
+
+// Write a function min(a,b) which returns the least of two numbers a and b.
+  function min(a, b) {
+   
+  }
+
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+  function pow(x, n) {
+    
+  }
